4ce6007ea886e14e074d011e022c82f3eb45975f
Regis JOUAN <regis.jouan@lapiscine.pro>
2024-11-22 18:38:30 +0100

    vercel tryout4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tous les fichiers React dans `src/`
+    "./src/styles/**/*.scss", // Tous les fichiers SCSS dans `styles/`
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
